0f19df3e41380bbb63e9989aa299e9d88f0b54e9
reshir18 <michael.raymond76@gmail.com>
2016-08-03 18:41:16 -0400

    Try Excel format
    ???

--- src/js/index.js
+++ src/js/index.js
@@ -47,6 +47,22 @@ doc.setFontSize(40);
 doc.text(30, 20, 'Hello world!');
 doc.addImage(imgData, 'JPEG', 15, 40, 66, 66);
 //doc.save('IMGTest.pdf');
+/*var excelbuilder = require('msexcel-builder');
+var workbook = excelbuilder.createWorkbook('./', 'sample.xlsx')
+ var sheet1 = workbook.createSheet('sheet1', 10, 12);
+  
+  // Fill some data 
+  sheet1.set(1, 1, 'I am title');
+  for (var i = 2; i < 5; i++)
+    sheet1.set(i, 1, 'test'+i);
+
+  // Save it
+  workbook.save(function(err){
+    if (err)
+      throw err;
+    else
+      console.log('congratulations, your workbook created');
+  });*/
 
 var Home = Vue.extend({
     ready: function (done) {document.title = "Accueil";},
